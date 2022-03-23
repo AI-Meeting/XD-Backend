8af57d93907672286068a6d9f798526380afbc18
Kimjimin0807 <jm004471@naver.com>
2022-03-23 14:35:28 +0900

    [CHORE] project setting

--- src/entities/Address.ts
+++ src/entities/Address.ts
@@ -4,22 +4,22 @@ import {
   Index,
   OneToMany,
   PrimaryGeneratedColumn,
-} from "typeorm";
-import { Company } from "./Company";
+} from 'typeorm';
+import { Company } from './Company';
 
-@Index("id_UNIQUE", ["id"], { unique: true })
-@Entity("address", { schema: "xddb" })
+@Index('id_UNIQUE', ['id'], { unique: true })
+@Entity('address', { schema: 'xddb' })
 export class Address {
-  @PrimaryGeneratedColumn({ type: "int", name: "id", unsigned: true })
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id', unsigned: true })
   id: number;
 
-  @Column("varchar", { name: "location", nullable: true, length: 255 })
+  @Column('varchar', { name: 'location', nullable: true, length: 255 })
   location: string | null;
 
-  @Column("int", { name: "latitude", nullable: true })
+  @Column('int', { name: 'latitude', nullable: true })
   latitude: number | null;
 
-  @Column("int", { name: "longitude", nullable: true })
+  @Column('int', { name: 'longitude', nullable: true })
   longitude: number | null;
 
   @OneToMany(() => Company, (company) => company.address)
--- src/entities/CommunityBoard.ts
+++ src/entities/CommunityBoard.ts
@@ -6,39 +6,39 @@ import {
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
-} from "typeorm";
-import { User } from "./User";
-import { CommunityComment } from "./CommunityComment";
+} from 'typeorm';
+import { User } from './User';
+import { CommunityComment } from './CommunityComment';
 
-@Index("id_UNIQUE", ["id"], { unique: true })
-@Index("fk_comunity_board_user1_idx", ["userId"], {})
-@Entity("community_board", { schema: "xddb" })
+@Index('id_UNIQUE', ['id'], { unique: true })
+@Index('fk_comunity_board_user1_idx', ['userId'], {})
+@Entity('community_board', { schema: 'xddb' })
 export class CommunityBoard {
-  @PrimaryGeneratedColumn({ type: "int", name: "id", unsigned: true })
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id', unsigned: true })
   id: number;
 
-  @Column("int", { name: "user_id", unsigned: true })
+  @Column('int', { name: 'user_id', unsigned: true })
   userId: number;
 
-  @Column("enum", {
-    name: "category",
-    enum: ["SCHOO", "COMPANY", "INTERVIEW", "ETC"],
+  @Column('enum', {
+    name: 'category',
+    enum: ['SCHOO', 'COMPANY', 'INTERVIEW', 'ETC'],
   })
-  category: "SCHOO" | "COMPANY" | "INTERVIEW" | "ETC";
+  category: 'SCHOO' | 'COMPANY' | 'INTERVIEW' | 'ETC';
 
-  @Column("varchar", { name: "content", length: 500 })
+  @Column('varchar', { name: 'content', length: 500 })
   content: string;
 
   @ManyToOne(() => User, (user) => user.communityBoards, {
-    onDelete: "NO ACTION",
-    onUpdate: "NO ACTION",
+    onDelete: 'NO ACTION',
+    onUpdate: 'NO ACTION',
   })
-  @JoinColumn([{ name: "user_id", referencedColumnName: "id" }])
+  @JoinColumn([{ name: 'user_id', referencedColumnName: 'id' }])
   user: User;
 
   @OneToMany(
     () => CommunityComment,
-    (communityComment) => communityComment.communityBoard
+    (communityComment) => communityComment.communityBoard,
   )
   communityComments: CommunityComment[];
 }

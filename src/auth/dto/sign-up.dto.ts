004e01f6341e82cc4ea0abfb9490a6663ad80410
Kimjimin0807 <jm004471@naver.com>
2022-03-28 14:12:52 +0900

    [ADD] 회원가입 요청 dto 추가

--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,17 +1,21 @@
-import { IsEmail, IsString, Length } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class SignUpDto {
   @IsString()
   @Length(3, 20)
+  @IsNotEmpty()
   readonly name: string;
 
   @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
 
   @IsString()
   @Length(4, 12)
+  @IsNotEmpty()
   readonly password: string;
 
   @IsString()
+  @IsNotEmpty()
   readonly school: string;
 }

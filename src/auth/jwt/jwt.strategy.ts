d4b7f1bfa4b1735fd59be9f14cd88244db791acb
Kimjimin0807 <jm004471@naver.com>
2022-03-28 20:12:03 +0900

    [ADD] jwt 설정 추가

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,21 +1,33 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Repository } from 'typeorm';
+import { User } from '../../entities/User';
 import { Payload } from './jwt.payload';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'secret',
+      secretOrKey: process.env.JWT_SECRET_KEY,
       ignoreExpiration: false,
     });
   }
 
   async validate(payload: Payload) {
-    const user = payload.id;
-
-    return user;
+    const user = await this.userRepository.findOne({
+      where: {
+        id: payload.sub,
+      },
+    });
+    if (user) {
+      return payload;
+    } else {
+      return BadRequestException;
+    }
   }
 }

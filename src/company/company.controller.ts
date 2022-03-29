1f9b812b5cdabe240171559ce8f42e2f352e7365
Kimjimin0807 <jm004471@naver.com>
2022-03-29 15:08:29 +0900

    [ADD] GET Company Location List Api Add

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { Company } from '../entities/Company';
 import { CompanyService } from './company.service';
 import { CompanyListResponseDto } from './dto/company-list-response.dto';
+import { CompanyLocationListResponseDto } from './dto/company-location-list-response.dto';
 
 @Controller('company')
 export class CompanyController {
@@ -12,5 +13,10 @@ export class CompanyController {
     @Query('name') name: string | undefined,
   ): Promise<CompanyListResponseDto[]> {
     return await this.companySerivce.getCompanyList(name);
+  }
+
+  @Get('list')
+  async getCompanyLocationList(): Promise<CompanyLocationListResponseDto[]> {
+    return await this.companySerivce.getCompanyLocationList();
   }
 }

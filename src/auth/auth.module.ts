806c444edee626f66a66b77a573ae9bff51ce690
Kimjimin0807 <jm004471@naver.com>
2022-03-29 10:03:28 +0900

    [FIX] JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -22,5 +22,6 @@ import { ConfigService } from '@nestjs/config';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy],
 })
 export class AuthModule {}

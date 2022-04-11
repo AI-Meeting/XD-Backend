97595db8889133a7fbfd252ac1b418495cdfa0bc
강은빈 <kub0803@naver.com>
2022-04-11 12:07:39 +0900

    [FIX] QuestionAnswerModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { APP_FILTER } from '@nestjs/core';
 import { CompanyModule } from './company/company.module';
 import { CommunityModule } from './community/community.module';
 import { InterviewModule } from './interview/interview.module';
+import { QuestionAnswerModule } from './question-answer/question-answer.module';
 import CatchException from './error/CatchException';
 
 @Module({
@@ -35,6 +36,7 @@ import CatchException from './error/CatchException';
     CompanyModule,
     CommunityModule,
     InterviewModule,
+    QuestionAnswerModule,
   ],
   controllers: [],
   providers: [

1caf20b345e39c2f171ff9e817ce39c63670180b
강은빈 <kub0803@naver.com>
2022-04-11 12:08:32 +0900

    [DELETE] interview 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { UserModule } from './user/user.module';
 import { APP_FILTER } from '@nestjs/core';
 import { CompanyModule } from './company/company.module';
 import { CommunityModule } from './community/community.module';
-import { InterviewModule } from './interview/interview.module';
 import { QuestionAnswerModule } from './question-answer/question-answer.module';
 import CatchException from './error/CatchException';
 
@@ -35,7 +34,6 @@ import CatchException from './error/CatchException';
     UserModule,
     CompanyModule,
     CommunityModule,
-    InterviewModule,
     QuestionAnswerModule,
   ],
   controllers: [],

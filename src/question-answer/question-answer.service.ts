9aa670a974a34b5193a0db80df23f84e2fff565e
강은빈 <kub0803@naver.com>
2022-05-31 23:52:49 +0900

    [FIX] questionAnswer aws bucket service

--- src/question-answer/question-answer.service.ts
+++ src/question-answer/question-answer.service.ts
@@ -7,6 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { QuestionAnswer } from '../entities/QuestionAnswer';
 import { QuestionAnswerPostDto } from './dto/question-answer-post.dto';
+import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class QuestionAnswerService {
@@ -17,17 +18,60 @@ export class QuestionAnswerService {
 
   async postQuestionAnswer(
     userId: number,
+    files: Array<Express.Multer.File>,
     questionId: number,
     body: QuestionAnswerPostDto,
   ) {
     const answer = new QuestionAnswer();
     answer.questionId = questionId;
     answer.answer = body.answer;
-    answer.videoUrl = body.videoUrl;
-    answer.voiceUrl = body.voiceUrl;
+
     answer.userId = userId;
 
-    await this.questionAnswerRepository.save(answer);
+    AWS.config.update({
+      region: 'ap-northeast-2',
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      },
+    });
+
+    try {
+      files.map(async (file, idx) => {
+        const date = Date.now();
+        const test = [];
+        // 버킷 저장
+        await new AWS.S3()
+          .putObject({
+            Key: `${date + file.originalname}`,
+            Body: file.buffer,
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            ACL: 'public-read',
+          })
+          .promise()
+          .then((res) => console.log(res));
+
+        // 버킷 URL 가져오기
+        await new AWS.S3().getSignedUrl(
+          'getObject',
+          {
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: `${date + file.originalname}`,
+          },
+          (err, url) => {
+            if (err) {
+              throw err;
+            }
+            console.log(url);
+            answer.videoUrl = url;
+          },
+        );
+      });
+      console.log(answer, test);
+      this.questionAnswerRepository.save(answer);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async deleteQuestionAanswer(userId: number, answerId: number) {

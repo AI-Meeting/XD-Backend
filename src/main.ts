ea26a4cc1af4a2f1856ef38ce41b4fc9c1b966f9
강은빈 <kub0803@naver.com>
2022-06-07 19:55:57 +0900

    [FEAT] question answer  영상 답변 등록 API 완성

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
--- src/question-answer/question-answer.service.ts
+++ src/question-answer/question-answer.service.ts
@@ -25,7 +25,6 @@ export class QuestionAnswerService {
     const answer = new QuestionAnswer();
     answer.questionId = questionId;
     answer.answer = body.answer;
-
     answer.userId = userId;
 
     AWS.config.update({
@@ -37,38 +36,60 @@ export class QuestionAnswerService {
     });
 
     try {
-      files.map(async (file, idx) => {
-        const date = Date.now();
-        const test = [];
-        // 버킷 저장
-        await new AWS.S3()
-          .putObject({
-            Key: `${date + file.originalname}`,
-            Body: file.buffer,
-            Bucket: process.env.AWS_S3_BUCKET_NAME,
-            ACL: 'public-read',
-          })
-          .promise()
-          .then((res) => console.log(res));
+      const date = Date.now();
 
-        // 버킷 URL 가져오기
-        await new AWS.S3().getSignedUrl(
-          'getObject',
-          {
-            Bucket: process.env.AWS_S3_BUCKET_NAME,
-            Key: `${date + file.originalname}`,
-          },
-          (err, url) => {
-            if (err) {
-              throw err;
-            }
-            console.log(url);
-            answer.videoUrl = url;
-          },
-        );
-      });
-      console.log(answer, test);
-      this.questionAnswerRepository.save(answer);
+      // 버킷 저장
+      await new AWS.S3()
+        .putObject({
+          Key: `${date + files[0].originalname}`,
+          Body: files[0].buffer,
+          Bucket: process.env.AWS_S3_BUCKET_NAME,
+          ACL: 'public-read',
+        })
+        .promise()
+        .then(async () => {
+          // 버킷 URL 가져오기
+          await new AWS.S3().getSignedUrl(
+            'getObject',
+            {
+              Bucket: process.env.AWS_S3_BUCKET_NAME,
+              Key: `${date + files[0].originalname}`,
+            },
+            (err, url) => {
+              if (err) {
+                throw err;
+              }
+              answer.voiceUrl = url;
+            },
+          );
+        });
+
+      await new AWS.S3()
+        .putObject({
+          Key: `${date + files[1].originalname}`,
+          Body: files[1].buffer,
+          Bucket: process.env.AWS_S3_BUCKET_NAME,
+          ACL: 'public-read',
+        })
+        .promise()
+        .then(async () => {
+          // 버킷 URL 가져오기
+          await new AWS.S3().getSignedUrl(
+            'getObject',
+            {
+              Bucket: process.env.AWS_S3_BUCKET_NAME,
+              Key: `${date + files[1].originalname}`,
+            },
+            (err, url) => {
+              if (err) {
+                throw err;
+              }
+
+              answer.videoUrl = url;
+              this.questionAnswerRepository.save(answer);
+            },
+          );
+        });
     } catch (error) {
       console.log(error);
     }
